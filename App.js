44213c73dcbfc554bb2c875da2b649aac2b3c488
Krispin Dolbear <kdolbear@mower.com>
2016-05-25 11:05:27 -0400

    add input updating headline

--- App.js
+++ App.js
@@ -6,15 +6,17 @@ class App extends React.Component {
 		this.state = { 
 			red: 0,
 			green: 0,
-			blue: 0
+			blue: 0,
+			name: ''
 		}
 		this.update = this.update.bind(this)
 	}
 	update(e) {
 		this.setState({
-			red: ReactDOM.findDOMNode(this.refs.red.refs.inp).value,
-			green: ReactDOM.findDOMNode(this.refs.green.refs.inp).value,
-			blue: ReactDOM.findDOMNode(this.refs.blue.refs.inp).value
+			red: ReactDOM.findDOMNode(this.refs.red.refs.sli).value,
+			green: ReactDOM.findDOMNode(this.refs.green.refs.sli).value,
+			blue: ReactDOM.findDOMNode(this.refs.blue.refs.sli).value,
+			name: ReactDOM.findDOMNode(this.refs.name.refs.txt).value
 		})
 	}
 	render() {
@@ -32,7 +34,8 @@ class App extends React.Component {
 				</div>
 				<hr />
 				<div>
-					<input type="text" />
+					<Input ref="name" update={this.update} />
+					<h1>Name: {this.state.name}</h1>
 				</div>
 			</div>
 		)
@@ -42,12 +45,22 @@ class App extends React.Component {
 class Slider extends React.Component {
 	render() {
 		return (
-				<div>
-					<input ref="inp" type="range"
-						min="0"
-						max="255"
-						onChange={this.props.update} />
-				</div>
+			<div>
+				<input ref="sli" type="range"
+					min="0"
+					max="255"
+					onChange={this.props.update} />
+			</div>
+		);
+	}
+}
+
+class Input extends React.Component {
+	render() {
+		return (
+			<div>
+				<input ref="txt" type="text" onChange={this.props.update} />
+			</div>
 		);
 	}
 }

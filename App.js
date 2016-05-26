26789e972d03a7e4260ae0e84bf04aa3850356f9
Krispin Dolbear <kdolbear@mower.com>
2016-05-26 13:19:01 -0400

    incrementing button on mount, clear interval on unmount

--- App.js
+++ App.js
@@ -109,17 +109,17 @@ class Third extends React.Component {
     this.setState({val: this.state.val +1})
   }
   componentWillMount() {
-    console.log('mounting')
+    this.setState({m: 2})
   }
   render() {
     console.log('rendering!')
-    return <button onClick={this.update}>{this.state.val}</button>
+    return <button onClick={this.update}>{this.state.val * this.state.m}</button>
   }
   componentDidMount() {
-    console.log('mounted')
+    this.inc = setInterval(this.update,500)
   }
   componentWillUnmount() {
-    console.log('bye!')
+    clearInterval(this.inc)
   }
 }
 
